36d1faeaf87cc7496cf810f6ba61628d6ef34bbc
Tungsten533 <36699528+Tungsten533@users.noreply.github.com>
2024-10-24 17:02:55 -0500

    revert a commit that broke everything

--- frontend/src/api/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-// src/api/axiosConfig.js
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust the base URL as needed
-});
-
-export default instance;
